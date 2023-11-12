4a7b291894c98de057e454a55c600e0236a52c50
Daph357 <a>
2023-11-12 20:04:12 +0100

    "Fix" that did nothing

--- Discord Classic/DCCInfoViewController.h
+++ Discord Classic/DCCInfoViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface DCCInfoViewController : UIViewController
+@interface DCCInfoViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
 
